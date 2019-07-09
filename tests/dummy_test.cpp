8072246473a08398cab71bf84836a8bb22bb9077
Stepan Orlov <majorsteve@mail.ru>
2019-07-10 01:20:21 +0300

    Added a test and docs build in CMake.

--- tests/dummy_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-// Tests factorial of 0.
-TEST(DummyTest) {
-//   EXPECT_EQ(Factorial(0), 1);
-    EXPECT_TRUE(true);
-}
-
---
+++ tests/test_VectorTemplate.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <cstdlib>
+#include "ode_num_int/VectorTemplate.h"
+
+using namespace ctm::math;
+
+TEST(VectorTemplate, IsInitiallyZero) {
+    Vector<double> v(10);
+    for (auto x : v)
+        EXPECT_EQ(x, 0);
+}
+
+TEST(VectorTemplate, CopiesCtorInput) {
+    std::vector<double> input_vector(10);
+    std::generate(input_vector.begin(), input_vector.end(), [](){ return 1.23*(rand() % 100); });
+
+    VectorData<double> input_VectorData(input_vector);
+    ASSERT_EQ(input_VectorData.size(), input_vector.size());
+    ASSERT_TRUE(std::equal(input_VectorData.begin(), input_VectorData.end(), input_vector.begin()));
+
+    Vector<double> v(input_VectorData);
+    ASSERT_EQ(v.size(), input_vector.size());
+    ASSERT_TRUE(std::equal(v.begin(), v.end(), input_vector.begin()));
+}
+
+// TODO: More tests covering VectorTemplate interface
