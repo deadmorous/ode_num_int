cb21e4b5c8bf974a3a3420d20ae284fc0f5960da
SkabinD <Skabin@list.ru>
2019-07-22 14:37:37 +0300

    second commit

--- tests/test_oderhs.cpp
+++ tests/test_oderhs.cpp
@@ -3,11 +3,6 @@
 #include <string>
 #include "ode_num_int/OdeRhs.h"
 #include "ode_num_int/BouncingBall.h"
-#include "ode_num_int/Oscillator.h"
-#include "ode_num_int/SlidingPoint.h"
-#include "ode_num_int/StiffOde1D.h"
-#include "ode_num_int/CoupledNonlinearOscillators.h"
-#include "ode_num_int/OptionalParameters.h"
 
 using namespace ctm::testmodels;
 
@@ -15,10 +10,18 @@ using namespace ctm::testmodels;
 TEST(OdeRhs, TestmodelsTest)
 {
     BouncingBall<VectorData<double>> BouncingBall;
-    ctm::OptionalParameters::Parameters ball, newParam, gravAccel; newParam["recovery_factor"] = 11; ctm::OptionalParameters G;
-    gravAccel["gravity_acceleration"] = 9.8; /*auto b = gravAccel["gravity_acceleration"];*/ double a = std::stod(gravAccel["gravity_acceleration"]);
-    ball = BouncingBall.parameters(); /*double a = ball["gravity_acceleration"]; */ double b = std::stod(ball["gravity_acceleration"]);
+    ctm::OptionalParameters::Parameters ball, newBall, forComparsion;
+    newBall["recovery_factor"] = 0.5;
+    forComparsion["gravity_acceleration"] = 9.8; forComparsion["recovery_factor"] = 0.5;
+
+    ball = BouncingBall.parameters();
+    double a = std::stod(forComparsion["gravity_acceleration"]);
+    double b = std::stod(ball["gravity_acceleration"]);
     ASSERT_EQ(a, b);
-//    BouncingBall.setParameters(newParam);
-//    ASSERT_EQ(ball["recobery_factor"], 11);
+
+    BouncingBall.setParameters(newBall);
+    ball = BouncingBall.parameters();
+    a = std::stod(forComparsion["recovery_factor"]);
+    b = std::stod(ball["recovery_factor"]);
+    ASSERT_EQ(a, b);
 }
